db4bd55cd0f3f6eabc208dd03dd22096606f7dd8
Andrii Klochko <klochko.andrew.official@gmail.com>
2022-11-30 16:30:55 +0200

    Implement Metropolis method.

--- app/controllers/distribution_controller.rb
+++ app/controllers/distribution_controller.rb
@@ -1,6 +1,7 @@
 require_relative '../../lib/ProbabilityDensityFunction'
 require_relative '../../lib/GeneratorCore'
 require_relative '../../lib/NeymanMethod'
+require_relative '../../lib/MetropolisMethod'
 
 class DistributionController < ApplicationController
   def index
@@ -17,12 +18,26 @@ class DistributionController < ApplicationController
       mu = mu.to_f
 
       generator = GeneratorCore.new(max_x, step, generation_count)
+      pdf_calculation_lambda = -> (x) { ProbabilityDensityFunction.solve(sigma, mu, x) }
 
       pdf_maximum_value = ProbabilityDensityFunction.maximum_value(sigma, mu)
-      pdf_calculation_lambda = -> (x) { ProbabilityDensityFunction.solve(sigma, mu, x) }
-      neyman_method_lambda = -> () { NeymanMethod.calculate(pdf_maximum_value, max_x, pdf_calculation_lambda) }
+      neyman_method_lambda = -> () { NeymanMethod.calculate(pdf_calculation_lambda, pdf_maximum_value, max_x) }
 
-      @calculation_result = generator.generate(neyman_method_lambda)
+      previous_method_result = 0
+      metropolis_method_lambda = -> () {
+        calculation_result = MetropolisMethod.calculate(pdf_calculation_lambda, max_x, previous_method_result)
+        previous_method_result = calculation_result
+        calculation_result
+      }
+
+      @calculation_result = {
+        'options' => {
+          'max_x' => max_x,
+          'step' => step,
+        },
+        'neymanMethod' => generator.generate(neyman_method_lambda),
+        'metropolisMethod' => generator.generate(metropolis_method_lambda)
+      }
     end
   end
 end
--- lib/NeymanMethod.rb
+++ lib/NeymanMethod.rb
@@ -1,7 +1,7 @@
 require_relative 'ProbabilityDensityFunction'
 
 class NeymanMethod
-  def self.calculate(maximum_value, right_boundary, function_callback)
+  def self.calculate(function_callback, maximum_value, right_boundary)
     while true
       x = rand(0.0..1.0) * right_boundary
       y = rand(0.0..1.0) * maximum_value
